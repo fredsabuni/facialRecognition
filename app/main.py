60d8b2ad165e3e291ad530ab6c8094dbc269b3ed
Fred Malack <fredmalack15@gmail.com>
2025-12-01 13:27:27 +0300

    Add static file serving for HTML assets

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from app.routers import recognition
 from app.database import init_db
+import os
+
 
 app = FastAPI(title="Facial Recognition Service")
 
@@ -14,6 +16,11 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Serve the folder where your HTML file is located
+STATIC_DIR = os.path.join(os.path.dirname(__file__), "..")
+
+app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
 
 app.include_router(recognition.router)
 
